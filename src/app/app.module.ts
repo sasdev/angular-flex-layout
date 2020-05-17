27c9218dfb7638cf53f7109ee480644bc7a54cd4
sasdev <wentzelheinrich@gmail.com>
2020-05-17 11:16:15 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,41 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+
 
 import { AppComponent } from './app.component'
 import { HelloComponent } from './hello.component'
 import { HeaderComponent } from './header/header.component'
 
-import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,FlexLayoutModule ],
+  imports:      [ 
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule
+    ],
   declarations: [ AppComponent, HelloComponent, HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
