a138128d6cb8991893aa069253cd3fd6bc9d0627
sasdev <wentzelheinrich@gmail.com>
2020-05-18 11:15:57 +0200

    1 file modified

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { Router, Route } from '@angular/router';
 
 @Component({
   selector: 'home',
-  templateUrl: './home.component',
+  templateUrl: './home.component.html',
   styleUrls: [ './home.component.css']
 })
 
