fd628bcc0ab0f057ad0e47b8993915b9546a880b
sasdev <wentzelheinrich@gmail.com>
2020-05-18 13:28:48 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component'
+
 import { StepperComponent } from './pages/stepper/stepper.component'
 import { HomeComponent } from './pages/home/home.component'
 
