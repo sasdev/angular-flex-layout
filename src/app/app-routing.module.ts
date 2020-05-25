2c0154bcd186eaedb92daa1752fc4f8782e2b604
sasdev <wentzelheinrich@gmail.com>
2020-05-25 15:00:22 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { StepperComponent } from './pages/stepper/stepper.component'
 import { HomeComponent } from './pages/home/home.component'
 import { Example1Component } from './pages/example1/example1.component'
+import { Example2Component } from './pages/example2/example2.component'
 import { FxFlexComponent } from './pages/fxflex/fxflex.component'
 
 
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'stepper', component: StepperComponent },
   { path: 'example1', component: Example1Component},
+  { path: 'example2', component: Example2Component},
   { path: 'fxflex', component: FxFlexComponent }
 
   // { path: 'companies', component: CompanyListComponent },
